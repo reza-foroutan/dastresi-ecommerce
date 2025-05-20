b44b4351193284bf327d193e7948c3636cf053c4
reza-foroutan <rezaforoutanjs@gmail.com>
2025-05-20 18:19:47 +0330

    version-000

---
+++ src/js/main.js
@@ -0,0 +1,14 @@
+// Initialize main home top slider Website
+var swiper = new Swiper(".mySwipermainhometopslider", {
+  cssMode: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  mousewheel: true,
+  keyboard: true,
+});
+// End Initialize main home top slider Website
